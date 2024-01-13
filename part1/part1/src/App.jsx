b6e3d73e1e84331aa5cc576a176c1a96cba9d2b2
drewbrownie21 <36270973+drewbrownie21@users.noreply.github.com>
2024-01-12 21:00:24 -0800

    Practicing Event Handling and State Caused Rendering
    Worked on event handling, state to child components and changes in state caused rendering.

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import { useState } from 'react'
 
-const App = (props) => {
-  const {counter} = props
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+  const increaseByOne = () => setCounter(counter+1)
+  const decreaseByOne = () => setCounter(counter-1)
+  const reset = () => setCounter(0)
+
   return (
-    <div>{counter}</div>
+    <div>
+      <div><Display counter={counter}/></div>
+
+      <Button onClick={increaseByOne} text={"Plus"}/>
+      <Button onClick={decreaseByOne} text={"Minus"}/>
+      <Button onClick={reset} text={"Reset"}/>
+
+
+    </div>
   )
 }
 
-export default App;
+export default App

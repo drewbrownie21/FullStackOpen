419b6f8ac086950c795adb0f6fd350230f3db428
drewbrownie21 <36270973+drewbrownie21@users.noreply.github.com>
2024-01-13 14:41:19 -0800

    Working on conditional rendering
    Practicing conditional rendering, and dealing with asynchronous commands

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -1,24 +1,55 @@
 import { useState } from 'react'
 
-const Display = ({counter}) => <div>{counter}</div>
+const History = (props) => {
+  if(props.allClicks.length === 0){
+    return(
+      <div>
+        This App is used by pressing buttons.
+      </div>
+    )
+  }
+  
+  return(
+    <div>
+      Button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
 
-const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
-  const increaseByOne = () => setCounter(counter+1)
-  const decreaseByOne = () => setCounter(counter-1)
-  const reset = () => setCounter(0)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right) 
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updatedRight = right + 1
+    setRight(updatedRight)
+    setTotal(left+updatedRight)
+  }
 
   return (
     <div>
-      <div><Display counter={counter}/></div>
-
-      <Button onClick={increaseByOne} text={"Plus"}/>
-      <Button onClick={decreaseByOne} text={"Minus"}/>
-      <Button onClick={reset} text={"Reset"}/>
-
-
+      {left}
+      <Button handleClick={handleLeftClick} text='Left'/>
+      <Button handleClick={handleRightClick} text='Right'/>
+      {right}
+      <History allClicks={allClicks}/>
+      <p>Total Clicks: {total}</p>
     </div>
   )
 }

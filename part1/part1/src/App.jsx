1e029e65df51bdd9431556e06c40c25f3c6550d2
drewbrownie21 <36270973+drewbrownie21@users.noreply.github.com>
2024-01-15 13:13:45 -0800

    Practice function returning functions
    Practicing functions returning a function

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -1,57 +1,22 @@
 import { useState } from 'react'
 
-const History = (props) => {
-  if(props.allClicks.length === 0){
-    return(
-      <div>
-        This App is used by pressing buttons.
-      </div>
-    )
+const App = () => {
+  const [value, setValue] = useState(10)
+
+  const setNewValue = (newValue) => () => {
+    setValue(newValue)
   }
-  
+
   return(
     <div>
-      Button press history: {props.allClicks.join(' ')}
+      {value}
+      <button onClick={setNewValue(1000)}>1000</button>
+      <button onClick={setNewValue(10)}>10</button>
+      <button onClick={setNewValue(value+1)}>Increment</button>
+
     </div>
   )
-}
 
-const Button = ({handleClick, text}) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
-
-const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-  const [total, setTotal] = useState(0)
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    const updatedLeft = left + 1
-    setLeft(updatedLeft)
-    setTotal(updatedLeft + right) 
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    const updatedRight = right + 1
-    setRight(updatedRight)
-    setTotal(left+updatedRight)
-  }
-
-  return (
-    <div>
-      {left}
-      <Button handleClick={handleLeftClick} text='Left'/>
-      <Button handleClick={handleRightClick} text='Right'/>
-      {right}
-      <History allClicks={allClicks}/>
-      <p>Total Clicks: {total}</p>
-    </div>
-  )
 }
 
 export default App

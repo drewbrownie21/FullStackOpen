641b137bb3921c7dd9f34f426a2d76487078cb12
drewbrownie21 <36270973+drewbrownie21@users.noreply.github.com>
2024-01-21 10:53:22 -0800

    Practice adding a new note to the existing list
    Practice adding a new note to this list. Added logic to dd note and handle the note change.

--- part2/part2_sandbox/src/App.jsx
+++ part2/part2_sandbox/src/App.jsx
@@ -1,8 +1,29 @@
 import { useState } from 'react'
-import './App.css'
 import Note from './components/Note'
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState(
+    'A new note...'
+  )
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      importanrt: Math.random < 0.5,
+      id : notes.length + 1
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote("")
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
   return (
     <div>
       <h1>Notes</h1>
@@ -11,6 +32,13 @@ const App = ({ notes }) => {
           <Note key={note.id} note={note} />
         )}
       </ul>
+      <form onSubmit={addNote}> 
+          <input 
+          value={newNote}
+          onChange={handleNoteChange}
+          />
+          <button type="submit">Save</button>
+      </form>
     </div>
   )
 }

7bb49570fb2aa959242c21a950e9dd31fadfbd86
drewbrownie21 <36270973+drewbrownie21@users.noreply.github.com>
2024-01-24 12:48:32 -0800

    Exercise 2.9 Step 4 Phonebook
    Exercise 2.9 step 4 of the phonebook exercise was to create a search function for the name.

--- part2/part2_exercises/phonebook/src/App.jsx
+++ part2/part2_exercises/phonebook/src/App.jsx
@@ -2,30 +2,35 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { 
-      name: 'Arto Hellas',
-      number: '040-1234567'
-  }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newSearch, setNewSearch] = useState('')
+
+  const peopleToShow = newSearch.length > 0
+    ? persons.filter(person => person.name.includes(newSearch))
+    : persons
 
   const addPerson = (event) => {
-    event.preventDefault()
+      event.preventDefault()
 
-    const personObject = {
-      name   : newName,
-      number : newNumber
-    }
+      const personObject = {
+        name   : newName,
+        number : newNumber
+      }
 
-    if(persons.map(a=>a.name).includes(newName)){
-      alert(`${newName} is already in the phonebook!`)
-    }
+      if(persons.map(a=>a.name).includes(newName)){
+        alert(`${newName} is already in the phonebook!`)
+      }
 
-    setPersons(persons.concat(personObject))
-    setNewName("")
-    setNewNumber("")
+      setPersons(persons.concat(personObject))
+      setNewName("")
+      setNewNumber("")
   }
 
   const handleNameChange = (event) => {
@@ -36,9 +41,20 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleSearch = (event) => {
+    setNewSearch(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter shown with a <input 
+        value={newSearch}
+        onChange={handleSearch}
+          />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input 
@@ -58,8 +74,8 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person, index) => 
-          <li key={index}>{person.name} {person.number}</li>
+        {peopleToShow.map(person => 
+          <li key={person.id}>{person.name} {person.number}</li>
         )}
       </ul>
     </div>

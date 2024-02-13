8dd52491c386fb5256264939e5663d25f3ad3cb8
drewbrownie21 <36270973+drewbrownie21@users.noreply.github.com>
2024-02-12 19:26:31 -0800

    Fixed folder structure
    Fixing folder structure by pulling phonebook out of the courses folder into its own folder under part 2 exercises.

--- part2/part2_sandbox/src/App.jsx
+++ part2/part2_sandbox/src/App.jsx
@@ -2,21 +2,29 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from './components/Note'
 import noteService from './services/notes'
+import './index.css'
+import Notification from './components/Notification'
+
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, Department of Computer Science, University of Helsinki 2024</em>
+    </div>
+  )
+}
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
-
-  const hook = () => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
-      })
-  }
+  const [errorMessage, setErrorMessage] = useState('Some error happened...')
   
   useEffect(() => {
     noteService
@@ -43,7 +51,6 @@ const App = () => {
   }
 
   const toggleImportanceOf = id => {
-    const url = `http://localhost:3001/notes/${id}`
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
   
@@ -53,7 +60,12 @@ const App = () => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
       .catch(error => {
-        alert(`the note '${note.content}' was already deleted from the server.`)
+        setErrorMessage(
+          `Note ${note.content} is already removed from the server!`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
       })
       setNotes(notes.filter(n => n.id !== id))
   }
@@ -70,6 +82,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message = {errorMessage}/>
       <ul>
         {notesToShow.map(note => 
           <Note 
@@ -86,6 +99,7 @@ const App = () => {
           />
           <button type="submit">Save</button>
       </form>
+      <Footer />
     </div>
   )
 }

ce3ed73cdce3096a4b2c7b883f99f708270fc539
drewbrownie21 <36270973+drewbrownie21@users.noreply.github.com>
2024-03-01 21:28:25 -0800

    Creating a frontend and backend for the Notes app
    Creating a frontend and backend for the Notes app to deploy to Render

--- part3/part3_sandbox/Notes_backend/index.js
+++ part3/part3_sandbox/Notes_backend/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const cors = require('cors')
+const PORT = process.env.PORT || 3001
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 let notes = [
@@ -69,7 +72,6 @@ app.post('/api/notes', (request, response) => {
     response.json(note)
   })
 
-const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })

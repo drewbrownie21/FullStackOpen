9fba7915a9dca1f0e7104cb028c777aaa4e66e7e
drewbrownie21 <36270973+drewbrownie21@users.noreply.github.com>
2024-03-06 20:46:45 -0800

    Exercise 3.11 - Step 11 - Phonebook
    Exercise 3.11 - Step 11, creating a production build with both the frontend and backend of the phonebook.

--- part3/part3_exercises/phonebook_backend/index.js
+++ part3/part3_exercises/phonebook_backend/index.js
@@ -6,6 +6,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 // Only set a body if the method was a POST request
 morgan.token('req-body', (req, res) => {

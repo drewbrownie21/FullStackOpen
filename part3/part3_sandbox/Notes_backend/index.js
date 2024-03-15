0e327e49dbc009ae2f65a59c0ae246f1b39ad890
drewbrownie21 <36270973+drewbrownie21@users.noreply.github.com>
2024-03-14 21:59:32 -0700

    Practice with integrating mongoDB with notes
    Added mongoDB to notes application.

--- part3/part3_sandbox/Notes_backend/index.js
+++ part3/part3_sandbox/Notes_backend/index.js
@@ -1,10 +1,12 @@
+require('dotenv').config()
+const Note = require('./models/notes')
 const express = require('express')
 const cors = require('cors')
 let morgan = require('morgan')
-
-const PORT = process.env.PORT || 3001
-
+const mongoose = require('mongoose')
 const app = express()
+
+const PORT = process.env.PORT
 
 app.use(cors())
 app.use(express.json())
@@ -12,71 +14,39 @@ app.use(express.static('dist')) // This makes express show static content
 // Logger using morgan - tiny 
 app.use(morgan('tiny'))
 
-
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-  ]
-
 app.get('/', (request, response) => {
     response.send('<h1>Hello World</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+        console.log(notes)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-
-    if(note){
-        response.json(note)
-    }else{
-        response.send('<h1>No id found</h1>')
-        response.status(400).end()
-    }
+    Note.findById(request.params.id).then(note => {
+      response.json(note)
+    })
 })
-
-const generate_id = () => {
-    const maxID = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-
-    return maxID + 1
-}
 
 app.post('/api/notes', (request, response) => {
     const body = request.body
 
-    if(!body.content){
-        return response.status(400).json({
-            error : "content missing"
-        })
+    if(body.content === undefined){
+        return response.status(400)
+            .json({error : "content missing"})
     }
 
-    const note = {
+    const note = new Note({
         content : body.content,
-        important : Boolean(body.important) || false,
-        id : generate_id()
-    }
+        important : Boolean(body.important) || false
+    })
 
-    notes = notes.concat(note)
-
-    response.json(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
   })
 
 app.listen(PORT, () => {
